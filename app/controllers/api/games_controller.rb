347e4695d63a3e1f1dd009ec48fd22e4bdb41b07
Heather Lee <heather.k.lee@gmail.com>
2018-06-27 20:24:33 -0400

    using params to show name and what happens if name begins with a

--- app/controllers/api/games_controller.rb
+++ app/controllers/api/games_controller.rb
@@ -1,6 +1,11 @@
 class Api::GamesController < ApplicationController
   def get_name
-    @message = "test"
+    # if params["name"][0].upcase == "A"
+    if params["name"][0] == "A" || params["name"][0] == "a"
+      @message = "Hey, your name starts with the first letter of the alphabet!"
+    else
+      @message = params["name"].upcase
+    end
     render "name_view.json.jbuilder"
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,6 @@ Rails.application.routes.draw do
   # STEP 1: A ROUTE triggers a controller action
   # verb "/urls" => "namespace/controllers#action"
   namespace :api do
-    get "/game" => 'games#get_name'
+    get '/game' => 'games#get_name'
   end
 end
